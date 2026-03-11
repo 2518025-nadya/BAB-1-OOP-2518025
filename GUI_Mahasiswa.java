2805d776568f9848b4e91c08cc6e8ab2e308cb02
2518025-nadya <2518025@student.itn.ac.id>
2026-03-11 09:07:31 +0700

    Add files via upload

---
+++ GUI_Mahasiswa.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BAB1ClassObject;
+
+/**
+ *
+ * @author Gavin
+ */
+public class event {
+    String nama, event,harga, nomor;
+    void dataNama(String nama){
+        this.nama = Nama;
+    }    
+    void dataEvent(String event){
+        this.event = Event;
+    }
+    void dataHarga(String harga){
+        this.harga = Harga;
+    }  
+    void dataNomor(String Nomor){
+        this.nomor = Nomor;
+    }  
+    
+    String cetakNama(){
+        return nama;
+    }
+    String cetakEvent(){
+        return event;
+    }
+    String cetakHarga(){
+        return Harga;
+    }
+    String cetakNomor(){
+        return nomor;
+    }
+}
