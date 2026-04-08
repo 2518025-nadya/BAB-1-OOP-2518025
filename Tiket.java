fd4e570e725e745714ad79187ebdbfc387cb3b91
2518025-nadya <2518025@student.itn.ac.id>
2026-04-08 20:12:06 +0700

    Add files via upload

---
+++ Tiket.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BAB3.Inheritance;
+
+public class EventEsport {
+    String namaEvent;
+    String game;
+    int jumlahPeserta;
+
+    void tampilEvent(){
+        System.out.println("Event: " + namaEvent);
+    }
+}
